0df7dcbdc3969780381d63f06bef08af8d55fc6b
Mudrank <mudrank.gupta.ind@gmail.com>
2021-04-15 17:56:17 +0530

    some styles

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello world👋</Text>
     </View>
   );
 }
@@ -17,5 +15,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#58355E'
   },
+  text:{
+    fontSize:30,
+    fontWeight: 'bold',
+    color: '#FFF689'
+  }
 });
